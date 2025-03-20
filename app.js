5b180a7be2e5b3fb323fa9a33a4fe7a9c3cb1c6c
kiruphathanan <kirupha2005@gmail.com>
2025-03-20 10:03:00 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+var REd=document.getElementById("on");
+var Yellow=document.getElementById("yellow");
+var green=document.getElementById("green");
+var imgR=document.getElementById("bulb");
+var imgY=document.getElementById("Y");
+var imgG=document.getElementById("g");
+
+function bulb_on(){
+    imgG.src="—Pngtree—white circle background_6044507.png"
+     imgY.src=" —Pngtree—white circle background_6044507.png"
+    imgR.src="A red circle with a face, 80px size, without background, like a sticker image.png";
+}
+function light_off(){
+       imgR.src=" —Pngtree—white circle background_6044507.png"
+       imgG.src="—Pngtree—white circle background_6044507.png"
+    imgY.src=" A yellow circle with a face, 80px size, without background, like a sticker image.png";
+}
+function gr(){
+    imgY.src="—Pngtree—white circle background_6044507.png"
+     imgY.src=" —Pngtree—white circle background_6044507.png"
+    imgG.src="A green circle with a smiling face, 80px size, without background, like a sticker image.png";
+}
+
+
+
+
+
+// A yellow circle with a face, 80px size, without background, like a sticker image.png
+// A green circle with a smiling face, 80px size, without background, like a sticker image.png
